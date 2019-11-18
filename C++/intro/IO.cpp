385cccfdb305e51001063d171cc4bd92d662e81e
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2019-11-17 17:26:04 -0800

    Create IO.cpp

---
+++ C++/intro/IO.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n1;
+    int n2;
+    int n3;
+
+    cin >> n1 >> n2 >> n3;
+    int sum = n1 + n2 + n3;
+    cout << sum;
+
+    return 0;
+}
