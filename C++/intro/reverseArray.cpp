80a941fbf1decf3043782f61f49ae32edcb840d9
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2019-11-17 17:25:04 -0800

    Create reverseArray.cpp

---
+++ C++/intro/reverseArray.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+    int numElements;
+    cin >> numElements;
+    int arr[numElements];
+    
+    for (int i = 0; i < numElements; i++){
+        cin >> arr[i];
+
+    }
+
+    for (int i = numElements -1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+
