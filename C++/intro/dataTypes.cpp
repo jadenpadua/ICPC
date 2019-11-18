a9eefcdcf753a31ec97f9181cefe67a7fe1367d2
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2019-11-17 17:26:49 -0800

    Create dataTypes.cpp

---
+++ C++/intro/dataTypes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+    printf("%d", i);
+    printf("\n");
+    printf("%ld", l);
+    printf("\n");
+    printf("%c", c);
+    printf("\n");
+    printf("%f", f);
+    printf("\n");
+    printf("%lf", d);
+    printf("\n");
+
+    // printf("%d",i, l);
+    // Complete the code.
+    return 0;
+}
