c87d6b3e812107f375c37e26dfe864a865e051d3
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-01-20 17:04:29 -0800

    Create maxSubArray.cpp

---
+++ C++/array/maxSubArray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxEndingHere = nums[0];
+        int maxSoFar = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            maxEndingHere = max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = max(maxEndingHere, maxSoFar);
+        }
+        
+        return maxSoFar;
+        
+    }
+};
