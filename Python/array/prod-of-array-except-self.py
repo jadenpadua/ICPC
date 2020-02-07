9979ba1ce19f19afdb6c84513f277986a54cd431
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-02-06 20:05:59 -0800

    Create prod-of-array-except-self.py

---
+++ Python/array/prod-of-array-except-self.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    #The approach here would be to multiply nums that come before and after said num
+        output = [1] * len(nums)
+        product = 1
+        for i in range(len(nums)):
+            #traversering the array in forward direction and multiplying to product
+            output[i] *= product
+            product *= nums[i]
+    
+        product = 1
+        for i in range(len(nums)-1,-1,-1):
+        #traversing the array in backward direction and multiplying to product
+            output[i] *= product
+            product *= nums[i]
+        return output
