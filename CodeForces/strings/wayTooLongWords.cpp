3d648f8544b06b78e48e8d0fbb108463dd62faf2
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-02-10 20:32:48 -0800

    Create wayTooLongWords.cpp

---
+++ CodeForces/strings/wayTooLongWords.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int n;
+    string word;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> word;
+        if (word.length() <= 10) {
+            cout << word << endl;
+            continue;
+        }
+        cout << word[0] + to_string(word.length()-2) + word[word.length()-1] << endl;
+    }
+    return 0;
+}
