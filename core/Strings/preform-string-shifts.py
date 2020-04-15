ea7024df24f05cb60bc951e82f812f328898afce
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-04-14 22:11:57 -0700

    Update preform-string-shifts.py

--- core/Strings/preform-string-shifts.py
+++ core/Strings/preform-string-shifts.py
@@ -1,1 +1,18 @@
 
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        s_list = list(s)
+        for i in range(len(shift)):
+            if shift[i][0] == 0:
+                for j in range(shift[i][1]):
+                    s_list.append(s_list.pop(0))
+            
+            else: 
+                for j in range(shift[i][1]):
+                    s_list.insert(0,s_list.pop())
+                
+                
+                
+        return ''.join(s_list)
+            
+                        
