a9bfb0b3cc6790b8e398a1ad6251721fd58efbbf
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-05-04 15:49:10 -0700

    Create find-compliment.py

---
+++ core/BitManipulation/find-compliment.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        mask = 1
+        while mask < num:
+            mask = (mask << 1) + 1
+            
+        return mask ^ num
