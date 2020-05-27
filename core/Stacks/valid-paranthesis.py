44f0a0fc893231c7bce2d29665205b5ae86cdf32
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-05-26 23:53:11 -0700

    Create valid-paranthesis.py

---
+++ core/Stacks/valid-paranthesis.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        ht = {"(":")", "{":"}", "[":"]"}
+        opening = "({[" 
+        closing = ")}]"
+        for i in range(len(s)):
+            if len(stack) == 0 and s[i] in closing:
+                return False
+            if s[i] in opening:
+                stack.append(s[i])
+            else:
+                if ht[stack.pop()] != s[i]:
+                    return False
+        return len(stack) == 0
