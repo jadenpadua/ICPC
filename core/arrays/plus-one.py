52091c9b913978ba5edc5611f272cb98c5a0771e
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-06-11 20:11:40 -0700

    Create plus-one.py

---
+++ core/arrays/plus-one.py
@@ -0,0 +1,9 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in reversed(range(len(digits))):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        
+        return [1] + digits
