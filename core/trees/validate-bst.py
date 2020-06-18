6375ca4bdcdd8cb1990fb7870e78b4f62ae42064
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-06-18 13:55:00 -0700

    Create validate-bst.py

---
+++ core/trees/validate-bst.py
@@ -0,0 +1,19 @@
+class BST:
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
+
+def validateBst(tree):
+	return validateBstHelper(tree, float("-inf"), float("inf"))
+
+def validateBstHelper(tree, minValue, maxValue):
+	if tree is None:
+		return True
+	
+	if tree.value < minValue or tree.value >= maxValue:
+		return False
+	leftIsValid = validateBstHelper(tree.left, minValue, tree.value)
+	rightIsValid = validateBstHelper(tree.right, tree.value, maxValue)
+	
+	return leftIsValid and rightIsValid
